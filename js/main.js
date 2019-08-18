9a594a3bc2242912623a299b9a393831cd0b28e4
RenatoWw <renatoflud2017@hotmail.com>
2019-08-18 20:48:39 -0300

    primeiro comite

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const background = document.querySelector('#background');
+const fontColor = document.querySelector('#font-color');
+const fontSize = document.querySelector('#font-size');
+const fontFamily = document.querySelector('#font-family');
+
+background.onchange = () => colorChange(backgroundSelectColor, background.value);
+fontColor.onchange = () => colorChange(fontSelectColor, fontColor.value);
+fontSize.onclick = () => {
+    const size = prompt('Enter the size:');
+    document.body.style.fontSize = size;
+}
+fontFamily.onclick = () => {
+    const family = prompt('Enter the family (arial, times, sans)')
+    document.body.style.fontFamily = family;
+}
+
+// CSS Functions
+
+const colorChange = (func, value) => {
+    switch(value) {
+        case 'white':
+            func('white');
+        break;
+
+        case 'purple':
+            func('purple');
+        break;
+
+        case 'yellow':
+            func('yellow');
+        break;
+
+        case 'green':
+            func('green');
+        break;
+
+        case 'black':
+            func('black');
+        break;
+        
+        case 'blue':
+            func('blue');
+        break;
+
+        case 'other':
+            func('other');
+        break;
+    }
+}
+
+const backgroundSelectColor = (color) => {
+    document.body.style.backgroundColor = color;
+    if(color == 'other') {
+        const other = prompt('Type the color');
+        document.body.style.backgroundColor = other;
+    }
+}
+
+const fontSelectColor = (color) => {
+    document.body.style.color = color;
+    if(color == 'other') {
+        const other = prompt('Type the color');
+        document.body.style.color = other;
+    }
+}
+
+// TODO: font family, font style
+// TODO: rgb and hex alues
+// TODO: create a html element, select it and customize it
+// TODO: image background
