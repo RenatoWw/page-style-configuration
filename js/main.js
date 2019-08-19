e98fcf0c0ae59fa332d6984d2d398c33fbe66fc2
RenatoWw <renatoflud2017@hotmail.com>
2019-08-19 19:19:50 -0300

    rgb value, styles and create a HTML element func

--- js/main.js
+++ js/main.js
@@ -2,22 +2,37 @@ const background = document.querySelector('#background');
 const fontColor = document.querySelector('#font-color');
 const fontSize = document.querySelector('#font-size');
 const fontFamily = document.querySelector('#font-family');
+const createElement = document.querySelector('#btnCreateElement');
+const elements = document.querySelector('#elements');
 
 background.onchange = () => backgroundSelectColor(background.value);
 fontColor.onchange = () => fontSelectColor(fontColor.value);
 fontSize.onclick = () => {
-    const size = prompt('Enter the size:');
+    const size = prompt('Enter the size (25px, 3em)');
     document.body.style.fontSize = size;
 }
 fontFamily.onclick = () => {
-    const family = prompt('Enter the family (arial, times, sans)')
+    const family = prompt('Enter the family (arial, times, sans)');
     document.body.style.fontFamily = family;
+}
+createElement.onclick = () => {
+    const element = prompt('Type here the element (h1, h2, p)');
+    const elementText = prompt('Type here some text');
+    const node = document.createElement(element);
+    const nodeText = document.createTextNode(elementText);
+    node.appendChild(nodeText);
+    node.classList.add('created-Element');
+    elements.appendChild(node);
 }
 
 // CSS Functions
 
 const backgroundSelectColor = (color) => {
     document.body.style.backgroundColor = color;
+    if(color == 'rgb') {
+        const rgb = prompt('Enter here the rgb value (255, 0, 0)');
+        document.body.style.backgroundColor = `rgb(${rgb})`;
+    }
     if(color == 'hex') {
         const hex = prompt('Enter here the hex value (#32a852)');
         document.body.style.backgroundColor = hex;
@@ -30,6 +45,10 @@ const backgroundSelectColor = (color) => {
 
 const fontSelectColor = (color) => {
     document.body.style.color = color;
+    if(color == 'rgb') {
+        const rgb = prompt('Enter here the rgb value (255, 0, 0)');
+        document.body.style.backgroundColor = `rgb(${rgb})`;
+    }
     if(color == 'hex') {
         const hex = prompt('Enter here the hex value (#32a852)');
         document.body.style.color = hex;
@@ -40,6 +59,5 @@ const fontSelectColor = (color) => {
     }
 }
 
-// TODO: rgb values
 // TODO: create a html element, select it and customize it
 // TODO: image background
